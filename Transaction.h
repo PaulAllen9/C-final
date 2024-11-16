dee4bf76d505c3d90ca6a5694870e643d817db93
PaulAllen9 <167917946+PaulAllen9@users.noreply.github.com>
2024-11-15 23:17:17 -0600

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -1,19 +1,26 @@
 #pragma once
 #include <string>
-#include "User.h"
-class Transaction
-{
+
+#include <chrono>
+#include <ctime>
+class Transaction {
 private:
-	User user;
+	//will be either withdraw or deposit
+	std::string action;
+	//the amount of the transaction
+	//all deposits will be positive,
+	//all withdrawls should be negative
+	int amount;
+
+	//will provide the date_time in format 
+	//day_of_week, month, number_day, hour:minute:sec, year
+	std::chrono::time_point<std::chrono::system_clock> date_time;
+
 public:
-	//constructor with action a
-	Transaction(int a);
+	Transaction(std::string action, int amount);
+	Transaction(int amount);
+	std::string get_action();
+	int get_amount();
+	std::string get_date_time();
 
-
-
-
-
-	void withdrawl();
-	void deposit();
 };
-
